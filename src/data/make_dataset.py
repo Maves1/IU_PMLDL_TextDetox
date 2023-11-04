78900830ca8eeda52c6fb3c4ae0ac60aece82357
Maves1 <seva21122@gmail.com>
2023-11-04 18:03:13 +0300

    fix: add folder creation

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,15 +1,19 @@
-import subprocess
+import subprocess, os
 
 dataset_link = "https://github.com/skoltech-nlp/detox/releases/download/emnlp2021/filtered_paranmt.zip"
-dataset_dest = "data/raw/filtered_paranmt.zip"
+dataset_dest = "data/raw/"
+dataset_name = "filtered_paranmt.zip"
+
+if not os.path.exists(dataset_dest):
+    os.makedirs(dataset_dest)
 
 try:
     print("Trying to download the dataset...")
-    subprocess.run(["wget", dataset_link, "-O", dataset_dest], check=True)
+    subprocess.run(["wget", dataset_link, "-O", dataset_dest + dataset_name], check=True)
     print("Dataset has been downloaded successfully to <project_folder>/data/raw/")
 
     print("Unzipping...")
-    subprocess.run(["unzip", dataset_dest, "-d", "data/raw/"])
+    subprocess.run(["unzip", dataset_dest + dataset_name, "-d", "data/raw/"])
     print("Dataset is ready!")
 except Exception:
     print("Something went wrong\nMake sure you run this script from the project root!")
